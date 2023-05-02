c6d74c20221d0038e34cf96a987c5627369a4691
keezhenyoong <53737510+keezhenyoong@users.noreply.github.com>
2023-05-02 07:37:41 +0000

    Push

---
+++ src/main/java/RewardsValue.java
@@ -0,0 +1,22 @@
+public class RewardsValue{
+
+  public double cashValue;
+  public double milesValue;
+
+  public setCashValue(double cashValue){
+    this.cashValue = cashValue;
+  }
+
+  public setMilesValue(double milesValue){
+    this.milesValue = milesValue;
+  }
+
+  public getCashValue(){
+    return cashValue;
+  }
+
+  public getMilesValue(){
+    return milesValue;
+  }
+  
+}
